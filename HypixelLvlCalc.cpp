aff049dab98cbd5b5249abcc32320d920595f253
Vader10 <42492061+Vader10@users.noreply.github.com>
2021-07-29 19:20:42 +0800

    Create HypixelLvlCalc.cpp

---
+++ HypixelLvlCalc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int curLvl;
+int goal;
+int curXP;
+int getTotalXP(int lvl){
+	int ans = 0;
+	int cnt = 10000;
+	for(int i = 1; i < lvl; i++){
+		ans += cnt;
+		cnt += 2500;
+	}
+	return ans;
+}
+int main(){
+	cout<<"What is your current Hypixel level? ";
+	cin>>curLvl;
+	cout<<endl;
+	cout<<"How much XP until the next level? ";
+	cin>>curXP;
+	cout<<endl;
+	cout<<"What is your goal? ";
+	cin>>goal;
+	cout<<endl;
+	cout<<"You are "<<getTotalXP(goal)-getTotalXP(curLvl+1)+curXP<<" XP away from level "<<goal<<". "<<endl;
+	return 0;
+}
